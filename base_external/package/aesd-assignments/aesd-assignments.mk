fbe582ec807dd5a2d36f51400d1b2f9840b60e25
Varghese Thomas <vthomas44@users.noreply.github.com>
2025-07-07 00:49:01 +0530

    assignment-9-complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,35 +6,43 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = a00f27c98f6315f2fe3124f1a8d59eee04621587
+AESD_ASSIGNMENTS_VERSION = befcca29318ee054c55bf631eacbb25ebd683bbd
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-vthomas44.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver
+AESD_ASSIGNMENTS_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+
+ifeq ($(BR2_PACKAGE_AESDCHAR),y)
+	MY_CFLAGS += -DUSE_AESD_CHAR_DEVICE=1
+else
+	MY_CFLAGS += -DUSE_AESD_CHAR_DEVICE=0
+endif
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-    $(MAKE) -C $(@D)/server \
-        CC="$(TARGET_CC)" \
-        LD="$(TARGET_CC)" \
-        CFLAGS="$(TARGET_CFLAGS)" \
-        LDFLAGS="$(TARGET_LDFLAGS)" \
-        clean
-
-    $(MAKE) -C $(@D)/server \
-        CC="$(TARGET_CC)" \
-        LD="$(TARGET_CC)" \
-        CFLAGS="$(TARGET_CFLAGS)" \
-        LDFLAGS="$(TARGET_LDFLAGS)" \
-        all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+    $(info ==== aesd-assignments.mk finder-app IS made ====)
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-    $(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
-    $(INSTALL) -d 0755 $(TARGET_DIR)/etc/init.d/
-    $(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	#$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer.sh $(TARGET_DIR)/usr/bin
+    $(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/* $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment9/* $(TARGET_DIR)/usr/bin
+    $(info ==== aesd-assignments.mk IS installed ====)
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
--- base_external/package/aesdchar/aesdchar.mk
+++
@@ -1,8 +0,0 @@
-AESDCHAR_VERSION = main
-AESDCHAR_SITE = https://github.com/cu-ecen-aeld/assignments-3-and-later-vthomas44.git
-AESDCHAR_SITE_METHOD = git
-AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
-AESDCHAR_LICENSE = Dual BSD/GPL
-
-$(eval $(kernel-module))
-$(eval $(generic-package))
